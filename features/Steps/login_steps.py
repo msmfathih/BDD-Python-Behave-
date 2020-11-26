37f943dbddb6f8ef3a0bf971aa8d4816de2c925e
msmfathih <msmfathih@users.noreply.github.com>
2020-11-26 14:01:49 +0400

    new login story added

--- features/Steps/login_steps.py
+++ features/Steps/login_steps.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 @given('start chrome browser')
 def startBrowser(context):
     context.driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -17,6 +16,22 @@ def navigateLoginPage(context):
 @then('verify the logo present on the page')
 def verifyLogo(context):
     status = context.driver.find_element(By.ID, 'logo').is_displayed()
+    assert status is True
+
+
+@then('Enter emailid "admin@gmail.com"')
+def enterEmail(context):
+    context.driver.find_element(By.ID, 'email').send_keys('admin@gmail.com')
+
+
+@then('Click on login button')
+def clickLogin(context):
+    context.driver.find_element(By.ID, 'enterimg').click()
+
+
+@then('User must login to the home page')
+def verifyPageTitle(context):
+    status = context.driver.find_element(By.ID, "//h1[contains(text(),'Automation Demo Site')]").is_displayed()
     assert status is True
 
 
--- features/demo_automation.feature
+++ features/demo_automation.feature
@@ -1,9 +1,12 @@
-Feature:Demo Automation Logo
+Feature:Demo Automation Login
 
-  Scenario: Logo present on the Demo Automation Page
+  Scenario: Login into the Demo Automation Page
     Given start chrome browser
     When open demo automation login page
     Then verify the logo present on the page
+    And Enter emailid "admin@gmail.com"
+    And Click on login button
+    Then User must login to the home page
     And close browser
 
 
